6f66f74b0c6c3d5a0c0f1b6798bc8923ca61470c
Rybakova Anna <elfochkaa@gmail.com>
2024-02-28 22:53:46 -0300

    clickomarket v1.6 коррект settings.py

--- clickomarket/clickomarket/settings.py
+++ clickomarket/clickomarket/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['185.182.110.254']
 
 
 # Application definition
